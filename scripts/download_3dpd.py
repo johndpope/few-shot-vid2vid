0403906d5b463f6925b9778531b99682294e156e
YouseiTakei <yousei_san@icloud.com>
2020-02-03 16:57:03 +0900

    add 3D People downloader

--- scripts/download_3dpd.py
+++ scripts/download_3dpd.py
@@ -1,37 +1,42 @@
-import urllib.request, requests, zipfile, os
+import requests, zipfile, os, sys, subprocess
+from tqdm import tqdm
+test_url = "https://cv.iri.upc-csic.es/Dataset/rgb/rgb_woman06_10.tar.gz"
+def main():
+    rgb_url = "https://cv.iri.upc-csic.es/Dataset/links/rgb.txt"  
+    seg_url = "https://cv.iri.upc-csic.es/Dataset/links/seg_cloth.txt" 
+    chpt_path = "./datasets"
+    rgb_dir = os.path.join(chpt_path, "cloth/train_images")
+    seg_dir = os.path.join(chpt_path, "cloth/train_labels")
+    dirs = [chpt_path, rgb_dir, seg_dir]  
+    ### make file
+    _=[os.makedirs(dir) for dir in dirs if not os.path.isdir(dir)] 
+    ### download_file
+    for url in [u for u in requests.get(rgb_url).text.split("\n") if u][:1]:  #Debug mode
+        download_file(url, rgb_dir)
+        #unzip_file()
 
-rgb_url = "https://cv.iri.upc-csic.es/Dataset/links/rgb.txt"
-seg_url = "https://cv.iri.upc-csic.es/Dataset/links/seg_cloth.txt"
-rgb_dir = "./datasets/cloth/train_images"  
-seg_dir = "./datasets/cloth/train_labels"  
-
-
+def download_file(url, dir='./'): 
+    session  = requests.Session()
+    response = session.get(url, stream=True)
+    destination  = os.path.join(dir, os.path.basename(url) )
+    content_size = int(response.headers["content-length"])
+    try:
+        print('download %s'%destination) 
+        CHUNK_SIZE = 32768
+        pbar = tqdm(total=content_size, unit="B", unit_scale=True)
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:
+                    pbar.update(len(chunk))
+                    f.write(chunk)
+        pbar.close()
+    except:
+        import traceback
+        traceback.print_exc()
 def unzip_file(file_name, unzip_path):
     zip_ref = zipfile.ZipFile(file_name, 'r')
     zip_ref.extractall(unzip_path)
     zip_ref.close()
     os.remove(file_name)
-
-def download_file(url, dir):
-    urls = requests.get(url).text.split("\n")
-    urls = urls[0]
-    for i, u in enumerate(urls):
-        urllib.request.urlretrieve(u, os.path.join(dir,"%s"%i)) 
-    
-
-def download_file_from_csic(id, destination):
-    print("download from %s and %s"%(rgb_url, seg_url))
-    download_file(rgb_url, rgb_dir)
-#    download_file(seg_url, seg_dir) 
-     
-
-def main():
-    file_id   = ''
-    chpt_path = './datasets/' 
-    if not os.path.isdir(chpt_path):
-        os.makedirs(chpt_path)
-    destination = os.path.join(chpt_path, 'datasets.zip')
-    download_file_from_csis(file_id, destination)
-#    unzip_file(destination, chpt_path)
-
-main() 
+if __name__=="__main__":
+    main() 
